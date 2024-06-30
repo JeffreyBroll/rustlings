a8257071e18044d9950908bc23eb1dd29c9de70a
Jeffrey M. Broll <broll@lanl.gov>
2024-06-30 17:28:09 -0600

    20240628

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,10 +1,12 @@
 // enums1.rs
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
+    Quit=0,
+    Echo=5,
+    Move=888,
+    ChangeColor=65536
     // TODO: define a few types of messages as used below
 }
 
